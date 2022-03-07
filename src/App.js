15345a12912206c245482e18e2b20cd81bdca462
Talha_Aydin <mt.aydin@outlook.com>
2022-03-07 20:39:43 +0300

    app and tree set

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useState } from 'react';
 import './App.scss';
+import Tree from './Components/Tree/Tree';
 
 function App() {
+  const [isShaked, setIsShaked] = useState(false);
+
+  const handleClick = () => {
+    setIsShaked(!isShaked);
+  };
+
   return (
     <div className="App">
-      <h1>App</h1>
+      <h1>Tree Shake</h1>
+      <Tree isShaked={isShaked}></Tree>
+      <div>
+        <button onClick={handleClick}>Shake</button>
+      </div>
     </div>
   );
 }
